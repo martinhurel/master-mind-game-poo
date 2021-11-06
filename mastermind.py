2a01f150d0653aa7ffba364a7a397f4ba4f3b2d0
Judy <j.soukkari@gmail.com>
2021-11-06 11:49:25 +0100

    choices circles

--- mastermind.py
+++ mastermind.py
@@ -7,12 +7,51 @@ pygame.init()
 
 WIDTH, HEIGHT = 800, 800
 TITLE_FONT = pygame.font.Font('mm_font.ttf',100)
-COLORS = ['blue', 'red', 'green', 'yellow', 'purple', 'white', 'orange']
+COLORS = ['blue', 'red', 'green', 'yellow', 'purple', 'white', 'orange', 'pink']
 white = (255,255,255)
 light_gray = (219, 219, 219)
 gray = (147, 147, 147)
 dark_gray = (84, 84, 84)
 red = (255, 54, 54)
+blue = (35, 0, 255)
+green = (0, 213, 65)
+yellow = (255, 195, 0)
+
+class RGB(Color):
+    def hex_format(self):
+        return '#{:02X}{:02X}{:02X}'.format(self.red,self.green,self.blue)
+
+class Game():
+
+    def __init__(self, difficulty = 2):
+        self.difficulty = difficulty
+        self.solution = random.sample(colors,4)
+        self.guesses = 0
+        self.right_places = 0
+
+    def play(self):
+        while self.guesses <= 10 and self.right_places != 4:
+            player_input = random.sample(colors,4) 
+            guess = Guess(player_input) 
+            print('Guess number',self.guesses,':',guess.guess)
+            self.right_places = guess.compare(self)[0]
+        print(self.solution)
+
+class Guess():
+    
+    def __init__(self, guess):
+        self.guess = guess
+
+    def compare(self, game):
+        right_place = 0
+        right_color = 0
+        game.guesses += 1
+        for i in self.guess:
+            if i == game.solution[self.guess.index(i)]:
+                right_place += 1
+            elif i in game.solution:
+                right_color += 1
+        return right_place, right_color
 
 window = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption('Mastermind') # Window name
@@ -43,15 +82,30 @@ def main():
         playing_rect = pygame.Surface((length,40))
         playing_rect.fill(gray)
         window.blit(playing_rect, (50, (50*11)+130))
+
+        slots_pos = []
         for y in range(1,5):
-            pygame.draw.circle(window, light_gray, ((length*y/5)+50,705), 20)
+            x = (length*y/5)+50
+            y = 705
+            pygame.draw.circle(window, light_gray, (x,y), 20)
 
-        choices = pygame.Surface((300,300))
-        choices.fill(white)
-        window.blit(choices, (450, 200))
+        choices_pos = []
+        for i in range(len(COLORS)):
+            x = (50*i)+400
+            y = 700
+            pygame.draw.circle(window, RGB(COLORS[i]), (x,y),20)
+            choices_pos.append((x,y))
 
-        
-
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
+            for position in choices_pos:
+                matching_x = (position[0] - 20 <= mouse_pos[0] <= position[0] + 20)
+                matching_y = (position[1] - 20 <= mouse_pos[1] <= position[1] + 20)
+                if matching_x & matching_y:
+                    index = choices_pos.index(position)
+                    print(COLORS[index])
+                pygame.draw.circle(window, light_gray, (x,y), 20)
+            
         pygame.display.update()
 
 def menu(surface):
@@ -59,6 +113,5 @@ def menu(surface):
         
 def game(surface):
     solution = random.sample(colors,4)
-
 
 main()
