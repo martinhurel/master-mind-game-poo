14ac223a585cff9286362c460e970844af86d239
martinhurel <martin.hurel@eemi.com>
2021-11-06 18:16:59 +0100

    Almost Done

--- mastermind.py
+++ mastermind.py
@@ -7,23 +7,7 @@ pygame.init()
 
 WIDTH, HEIGHT = 800, 800
 TITLE_FONT = pygame.font.Font('mm_font.ttf',100)
-COLORS = ['blue', 'red', 'green', 'yellow', 'purple', 'white', 'orange', 'pink']
-white = (255,255,255)
-light_gray = (219, 219, 219)
-gray = (147, 147, 147)
-dark_gray = (84, 84, 84)
-red = (255, 54, 54)
-blue = (35, 0, 255)
-green = (0, 213, 65)
-yellow = (255, 195, 0)
-
-
-class RGB(Color):
-    def hex_format(self):
-        return '#{:02X}{:02X}{:02X}'.format(self.red,self.green,self.blue)
-
-colorInit = [RGB('grey')]*4
-indexToChange = 0
+COLORS = ['blue', 'red', 'green', 'yellow', 'purple', 'orange', 'pink']
 
 class Guess():
     
@@ -55,6 +39,8 @@ class Game():
         # Create all the 10 line
         self.line = [[RGB('red')]*4]*10
 
+        self.answerClue = [[RGB('blue')]*4]*10
+
         # Create the line wich the player will modify
         self.linePlayer = [RGB('grey')]*4
 
@@ -63,6 +49,20 @@ class Game():
 
         # Line number that has been validate
         self.linePlayerPosition = 0
+
+        self.answer = self.create_answer()
+
+        self.finish = False
+
+    def create_answer(self):
+        # Pick 4 random number between 0 and 3 (compris)
+
+        answer = []
+
+        for i in range(0,4):
+            answer.append(RGB(COLORS[i]))
+
+        return answer
 
     def changeLinePlayer(self, color):
         if self.elementPlayerPosition < 4:
@@ -77,45 +77,69 @@ class Game():
             pygame.draw.circle(window, self.linePlayer[idx], (x,y), 20)
 
     def createLine(self):
-        if self.line <= 9:
+        if self.linePlayerPosition <= 9:
             for i, val in enumerate(self.line):
                 length = 300
                 rect = pygame.Surface((length,40))
-                pygame.draw.rect(rect, gray, rect.get_rect(), border_radius = 25)
+                rectAnswer = pygame.Surface((length,40))
+                
+                # Answer rectangle
+                pygame.draw.rect(rect, RGB('grey'), rect.get_rect(), border_radius = 25)
+
+                # Clue rectangle
+                pygame.draw.rect(rectAnswer, RGB('white'), rectAnswer.get_rect())
 
                 for y, valArray in enumerate(val):
+                    # Answer Space
                     circle = pygame.Surface((30,30))
-                    circle.fill(gray)
+                    circle.fill(RGB('grey'))
                     pygame.draw.circle(circle, valArray, (circle.get_width()/2,circle.get_height()/2), 15)
                     rect.blit(circle, (60+(50*y),5))
+
+                    # Clue Space
+                    circle = pygame.Surface((30,30))
+                    circle.fill(RGB('white'))
+                    pygame.draw.circle(circle, self.answerClue[len(self.answerClue) - (i+1)][y], (circle.get_width()/2,circle.get_height()/2), 15)
+                    rectAnswer.blit(circle, (60+(50*y),5))
                 
                 window.blit(rect, (50, (50*i)+130))
+                window.blit(rectAnswer, (length + 70, (50*i)+130))
             
-            playing_rect = pygame.Surface((length,40))
-            playing_rect.fill(gray)
+            playing_rect = pygame.Surface((length,47))
+            playing_rect.fill(RGB('white'))
             window.blit(playing_rect, (50, (50*11)+130))
 
     def validate(self):
         if self.linePlayer != [RGB('grey')]*4:
             self.line[(len(self.line)-1) - self.linePlayerPosition] = self.linePlayer
-            self.linePlayer = [RGB('grey')]*4
             self.linePlayerPosition += 1
             self.elementPlayerPosition = 0
+            self.checkLineValidation()
+            self.linePlayer = [RGB('grey')]*4
+
     
-    def checkLineValidation(self):
-        # RULE
-        
-        # Si la combinaison présentée est incorrecte :
+    def checkLineValidation(self):    
+        clue = []
+        for idx, el in enumerate(self.linePlayer):
+            if self.answer[idx] == el:
+                clue.append(RGB('red'))
+            elif el in self.answer:
+                clue.append(RGB('black'))
+            else:
+                clue.append(RGB('white'))
 
-        # Le joueur qui a élaboré la combinaison secrète utilise les languettes situé sur les côtés du plateau.
-        # Si dans la proposition, un ou plusieurs pions de couleurs sont bien dans la combinaison mais pas à la bonne place, le joueur doit alors tirer la languette blanche selon le nombre.
-        # Si dans la proposition, un ou plusieurs pions de couleurs sont bien dans la combinaison et à la bonne place, le joueur doit alors tirer la languette rouge selon le nombre.
-        # Le joueur devant deviner la combinaison continue ainsi en proposant sur la seconde ligne une autre proposition, en prenant bien entendu en compte les indications des languettes rouges et blanches. Il a droit a 12 propositions pour déchiffrer le code.
+        self.answerClue[self.linePlayerPosition - 1] = clue
 
-        # Si la combinaison est correcte :
+        if clue == [RGB('red')]*4:
+            self.win()
 
-        # Le joueur qui a élaboré la combinaison secrète révèle le code et la manche est terminée. Les rôles sont alors inversés et la seconde manche peut ainsi commencer.
-
+    def win(self):
+        smallfont = pygame.font.SysFont('Corbel',35)
+        text = smallfont.render('Bravo Marcel tu as gagné' , True , RGB('white'))
+        text2 = smallfont.render('Ton score est ' +  str(10 - self.linePlayerPosition), True , RGB('white'))
+        window.blit(text, (175, 740))
+        window.blit(text2, (175, 800))
+        self.finish = True
 
 def main():
     gameExit = False
@@ -124,47 +148,41 @@ def main():
     # Create button validation
     smallfont = pygame.font.SysFont('Corbel',35)
     text = smallfont.render('Validé' , True , RGB('white'))
-    window.blit(text, (600, 600))
+    window.blit(text, (175, 740))
 
     while not gameExit:
         for event in pygame.event.get(): 
             if event.type == QUIT:
                 gameExit = True
-        menu(window)
-        game.createLine()
+        draw_text('MASTERMIND', TITLE_FONT, (255, 255, 255), window, (WIDTH/4)+50, 20)
 
-        slots_pos = []
+        if game.finish == False:
+            game.createLine()   
+            game.createColor()
 
-        game.createColor()
+            choices_pos = []
+            for i in range(len(COLORS)):
+                x = (50*i)+400
+                y = 700
+                pygame.draw.circle(window, RGB(COLORS[i]), (x,y),20)
+                choices_pos.append((x,y))
 
-        choices_pos = []
-        for i in range(len(COLORS)):
-            x = (50*i)+400
-            y = 700
-            pygame.draw.circle(window, RGB(COLORS[i]), (x,y),20)
-            choices_pos.append((x,y))
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_pos = pygame.mouse.get_pos()
-            for position in choices_pos:
-                matching_round_x = (position[0] - 20 <= mouse_pos[0] <= position[0] + 20)
-                matching_round_y = (position[1] - 20 <= mouse_pos[1] <= position[1] + 20)
-                if matching_round_x & matching_round_y:
-                    index = choices_pos.index(position)
-                    game.changeLinePlayer(COLORS[index])
-                matching_text_x = (600 <= mouse_pos[0] <= 650)
-                matching_text_y = (600 <= mouse_pos[1] <= 630)
-                if matching_text_x & matching_text_y:
-                    game.validate()
-                    
-        pygame.display.update()
-        clock = pygame.time.Clock()
-        clock.tick(10)
-
-def menu(surface):
-    draw_text('MASTERMIND', TITLE_FONT, (255, 255, 255), surface, (WIDTH/4)+50, 20)
-        
-def game(surface):
-    solution = random.sample(colors,4)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                for position in choices_pos:
+                    matching_round_x = (position[0] - 20 <= mouse_pos[0] <= position[0] + 20)
+                    matching_round_y = (position[1] - 20 <= mouse_pos[1] <= position[1] + 20)
+                    if matching_round_x & matching_round_y:
+                        index = choices_pos.index(position)
+                        game.changeLinePlayer(COLORS[index])
+                    matching_text_x = (175 <= mouse_pos[0] <= 225)
+                    matching_text_y = (740 <= mouse_pos[1] <= 770)
+                    if matching_text_x & matching_text_y:
+                        game.validate()
+                        
+            pygame.display.update()
+            clock = pygame.time.Clock()
+            clock.tick(10)
+        else:
 
 main()
